4cc0cd00a4115143056eac0245697268fa53f881
DavidSon29-code <Davidson24627@gmail.com>
2025-04-15 14:56:44 +0800

    Add project files.

---
+++ Kopee/LogInForm.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Kopee
+{
+    partial class LogInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.register = new System.Windows.Forms.Button();
+            this.ShowPassword = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Con_Reg = new System.Windows.Forms.TextBox();
+            this.Password_Reg = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Username_Reg = new System.Windows.Forms.TextBox();
+            this.Email_Reg = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Controls.Add(this.Email);
+            this.guna2CustomGradientPanel1.Controls.Add(this.Email_Reg);
+            this.guna2CustomGradientPanel1.Controls.Add(this.pictureBox2);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label7);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label4);
+            this.guna2CustomGradientPanel1.Controls.Add(this.button1);
+            this.guna2CustomGradientPanel1.Controls.Add(this.register);
+            this.guna2CustomGradientPanel1.Controls.Add(this.ShowPassword);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label3);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label2);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label5);
+            this.guna2CustomGradientPanel1.Controls.Add(this.label1);
+            this.guna2CustomGradientPanel1.Controls.Add(this.Con_Reg);
+            this.guna2CustomGradientPanel1.Controls.Add(this.Password_Reg);
+            this.guna2CustomGradientPanel1.Controls.Add(this.pictureBox1);
+            this.guna2CustomGradientPanel1.Controls.Add(this.Username_Reg);
+            this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2CustomGradientPanel1.FillColor = System.Drawing.Color.Gold;
+            this.guna2CustomGradientPanel1.FillColor2 = System.Drawing.Color.PaleGoldenrod;
+            this.guna2CustomGradientPanel1.FillColor3 = System.Drawing.Color.Yellow;
+            this.guna2CustomGradientPanel1.FillColor4 = System.Drawing.Color.LemonChiffon;
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2CustomGradientPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(428, 714);
+            this.guna2CustomGradientPanel1.TabIndex = 2;
+            this.guna2CustomGradientPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2CustomGradientPanel1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Kopee.Properties.Resources.close;
+            this.pictureBox2.Location = new System.Drawing.Point(365, 12);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(51, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DimGray;
+            this.label7.Location = new System.Drawing.Point(148, 657);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "back to LOGIN";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(113, 637);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Already have an acount?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(76, 589);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 34);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // register
+            // 
+            this.register.BackColor = System.Drawing.Color.Yellow;
+            this.register.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.register.FlatAppearance.BorderSize = 0;
+            this.register.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.register.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register.Location = new System.Drawing.Point(76, 530);
+            this.register.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.register.Name = "register";
+            this.register.Size = new System.Drawing.Size(269, 34);
+            this.register.TabIndex = 9;
+            this.register.Text = "REGISTER";
+            this.register.UseVisualStyleBackColor = false;
+            this.register.Click += new System.EventHandler(this.register_Click);
+            // 
+            // ShowPassword
+            // 
+            this.ShowPassword.AutoSize = true;
+            this.ShowPassword.BackColor = System.Drawing.Color.Transparent;
+            this.ShowPassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ShowPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowPassword.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowPassword.Location = new System.Drawing.Point(236, 500);
+            this.ShowPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ShowPassword.Name = "ShowPassword";
+            this.ShowPassword.Size = new System.Drawing.Size(136, 21);
+            this.ShowPassword.TabIndex = 8;
+            this.ShowPassword.Text = "Show Password";
+            this.ShowPassword.UseVisualStyleBackColor = false;
+            this.ShowPassword.CheckedChanged += new System.EventHandler(this.ShowPassword_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(33, 431);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Confirm Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(33, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label5.Location = new System.Drawing.Point(30, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Get Started";
+            // 
+            // Con_Reg
+            // 
+            this.Con_Reg.BackColor = System.Drawing.Color.White;
+            this.Con_Reg.Location = new System.Drawing.Point(37, 457);
+            this.Con_Reg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Con_Reg.Multiline = true;
+            this.Con_Reg.Name = "Con_Reg";
+            this.Con_Reg.PasswordChar = '*';
+            this.Con_Reg.Size = new System.Drawing.Size(336, 30);
+            this.Con_Reg.TabIndex = 0;
+            // 
+            // Password_Reg
+            // 
+            this.Password_Reg.BackColor = System.Drawing.Color.White;
+            this.Password_Reg.Location = new System.Drawing.Point(36, 387);
+            this.Password_Reg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Password_Reg.Multiline = true;
+            this.Password_Reg.Name = "Password_Reg";
+            this.Password_Reg.PasswordChar = '*';
+            this.Password_Reg.Size = new System.Drawing.Size(336, 30);
+            this.Password_Reg.TabIndex = 0;
+            this.Password_Reg.TextChanged += new System.EventHandler(this.textPassword_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Kopee.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Username_Reg
+            // 
+            this.Username_Reg.BackColor = System.Drawing.Color.White;
+            this.Username_Reg.Location = new System.Drawing.Point(34, 314);
+            this.Username_Reg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Username_Reg.Multiline = true;
+            this.Username_Reg.Name = "Username_Reg";
+            this.Username_Reg.Size = new System.Drawing.Size(336, 32);
+            this.Username_Reg.TabIndex = 0;
+            // 
+            // Email_Reg
+            // 
+            this.Email_Reg.BackColor = System.Drawing.Color.White;
+            this.Email_Reg.Location = new System.Drawing.Point(34, 246);
+            this.Email_Reg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Email_Reg.Multiline = true;
+            this.Email_Reg.Name = "Email_Reg";
+            this.Email_Reg.Size = new System.Drawing.Size(336, 32);
+            this.Email_Reg.TabIndex = 12;
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.BackColor = System.Drawing.Color.Transparent;
+            this.Email.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Email.Location = new System.Drawing.Point(33, 221);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(66, 23);
+            this.Email.TabIndex = 13;
+            this.Email.Text = "Email";
+            this.Email.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // LogInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 714);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "LogInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogInForm";
+            this.guna2CustomGradientPanel1.ResumeLayout(false);
+            this.guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private System.Windows.Forms.TextBox Username_Reg;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Con_Reg;
+        private System.Windows.Forms.TextBox Password_Reg;
+        private System.Windows.Forms.CheckBox ShowPassword;
+        private System.Windows.Forms.Button register;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox Email_Reg;
+        private System.Windows.Forms.Label Email;
+    }
+}
---
+++ Kopee/LogInForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Kopee
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\RenzP\\OneDrive\\Documents\\FrontDb.mdf;Integrated Security=True;Connect Timeout=30");
+        private void label7_Click(object sender, EventArgs e)
+        {
+            GetStarted gt = new GetStarted();
+            gt.Show();
+            this.Hide();
+        }
+
+        private void register_Click(object sender, EventArgs e)
+        {
+            GetStarted gt1 = new GetStarted();
+            gt1.Show();
+            this.Hide();
+            // 1.
+            try
+            {
+                if (Email_Reg.Text!="" && Username_Reg.Text != "" && Password_Reg.Text != "" && Con_Reg.Text != "")
+                {
+                    if (Password_Reg.Text == Con_Reg.Text )
+                    {
+                        // 3.
+                        int v = check(Email_Reg.Text);
+                        if (v!=1)
+                        {
+                            connection.Open();
+                            SqlCommand command = new SqlCommand("insert into RegistrationTbl values(@email,@username,@password)", connection);
+                            command.Parameters.AddwithValue("@Email", Email_Reg.Text);
+                            command.Parameters.AddwithValue("@Username", Username_Reg.Text);
+                            command.Parameters.AddwithValue("@Password", Password_Reg.Text);
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                            MessageBox.Show("Registered Successfully!");
+
+                            Email_Reg.Text = "";
+                            Username_Reg.Text = "";
+                            Password_Reg.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Email is already registered");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password do not match");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please fill all the blanks");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        // 2.
+        int check(string email)
+        {
+            connection.Open();
+            string query = "select count(*) from RegistrationTbl where email='" + email + "'";
+            SqlCommand command = new SqlCommand(query, connection);
+            int v = (int)command.ExecuteScalar();
+            connection.Close();
+            return v;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void guna2CustomGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ShowPassword.Checked)
+            {
+                Password_Reg.UseSystemPasswordChar = true;
+                Con_Reg.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                Password_Reg.UseSystemPasswordChar = false;
+                Con_Reg.UseSystemPasswordChar = false;
+            }
+        }
+    }
+}
